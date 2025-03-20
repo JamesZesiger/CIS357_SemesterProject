fd4ef9965212c2c70d34e2714e0d149295208d1e
JamesZesiger <zesigeja@mail.gvsu.edu>
2025-03-20 16:12:28 -0400

    Init commit

---
+++ WorkFolio/WorkFolio/WorkFolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkFolioApp.swift
+//  WorkFolio
+//
+//  Created by James R. Zesiger on 3/20/25.
+//
+
+import SwiftUI
+
+@main
+struct WorkFolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
