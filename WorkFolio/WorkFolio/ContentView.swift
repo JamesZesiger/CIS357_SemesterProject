a0e9f08ac8679baa6e07f5c649a4b154007d16c0
James <Zesigeja@mail.gvsu.edu>
2025-03-21 08:17:26 -0400

    Init Commit

---
+++ WorkFolio/WorkFolio/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WorkFolio
+//
+//  Created by James R. Zesiger on 3/21/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
